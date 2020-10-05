b3bf6654b3f61cdbf30cffceacb5fff17ac34e66
Adam Kewley <contact@adamkewley.com>
2020-10-05 18:26:41 +0100

    Added assimp etc.

---
+++ src/logl_13_model-loading.cpp
@@ -0,0 +1,8 @@
+#include <assimp/Importer.hpp>
+#include <assimp/scene.h>
+#include <assimp/postprocess.h>
+
+int main(int, char**) {
+    // TODO: need to do L12_multiple-lights first
+    return 0;
+}
